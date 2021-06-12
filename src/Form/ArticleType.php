c5a36097af892bbb18e1f95025a766dda7b186f4
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-06-12 14:04:23 +0100

    version avec bootstrap + crud provider + crud article + ManyToOne

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Provider;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('prix')
+            ->add('quantite')
+            ->add('provider', EntityType::class,[
+           
+            'class' => Provider::class,
+            'choice_label' => 'name',
+            'placeholder' => '-',
+            
+        ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
