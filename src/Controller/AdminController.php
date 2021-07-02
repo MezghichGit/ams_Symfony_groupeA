f84278252cd78f66e8dec50ffc3046fc4ae79209
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-07-02 16:54:26 +0100

    ajax exemple

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,7 +10,7 @@ use App\Repository\ProviderRepository;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/", name="admin")
+     * @Route("/admin", name="admin")
      */
     public function index(ProviderRepository $providerRepository): Response
     {
@@ -20,5 +20,16 @@ class AdminController extends AbstractController
         return $this->render('admin/index.html.twig', [
              'total'=>$providerRepository->totalProviders()
         ]);
+    }
+
+     /**
+     * @Route("/", name="/home")
+     */
+    public function home(): Response
+    {
+         
+
+        return new Response("<h1 align=center>Sesame University - INGTA-Groupe A - Juin 2021<h1>");
+        
     }
 }
